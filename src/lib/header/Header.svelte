94573550010b22d09ce9f67fcc66cbf07cb4bb1d
Dan Hahn <daniel.hahn@onepeloton.com>
2022-02-15 11:01:38 -0500

    move to svelte kit and tailwind

---
+++ src/lib/header/Header.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import logo from './svelte-logo.svg';
+</script>
+
+<header>
+	<ul>
+		<li>
+			<a href="/">Home</a>
+		</li>
+		<li>
+			<a href="/about">About</a>
+		</li>
+	</ul>
+</header>
---
+++ src/routes/index.svelte
@@ -0,0 +1,13 @@
+<script context="module" lang="ts">
+	export const prerender = true;
+</script>
+
+<script lang="ts">
+	import Counter from '$lib/Counter.svelte';
+</script>
+
+<svelte:head>
+	<title>Home</title>
+</svelte:head>
+
+<h1 class="text-3xl font-bold underline text-center">Hello world!</h1>
